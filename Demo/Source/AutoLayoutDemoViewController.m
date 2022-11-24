a2e40ed81afdb36d003cb33bf86ed4fb3e3b5d04
weiniu <venn221@gmail.com>
2022-11-24 14:56:55 +0800

    changed: 修改测试代码以及描述文件

--- Demo/Source/AutoLayoutDemoViewController.m
+++ Demo/Source/AutoLayoutDemoViewController.m
@@ -19,9 +19,23 @@
 
     [super viewDidLoad];
 
-    NSString *html = @"<html><body>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</body></html>";
+    NSString *html = @"<html><body>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <b>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea </b>commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</body></html>";
     NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
-    self.textView.attributedString = [[NSAttributedString alloc] initWithHTMLData:data documentAttributes:NULL];
+	
+	
+	NSDictionary *options = @{
+		DTUseiOS6Attributes:@YES, // 这个属性设置为NO就会崩溃 `-[__NSCFType hyphenationFactor]: unrecognized selector sent to instance`
+		DTIgnoreInlineStylesOption:@YES,
+		DTDefaultLinkDecoration:@NO,
+		DTDefaultLinkColor:[UIColor blueColor],
+		DTLinkHighlightColorAttribute:[UIColor redColor],
+		DTDefaultFontSize:@15,
+		DTDefaultFontFamily:@"Helvetica Neue",
+		DTDefaultFontName:@"HelveticaNeue-Light"
+	};
+
+	self.textView.attributedString = [[NSAttributedString alloc] initWithHTMLData:data options:options documentAttributes:nil];
+	
 }
 
 @end
